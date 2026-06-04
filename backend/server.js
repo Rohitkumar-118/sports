690f79f7c3e31144b14caaf3d8e5248529114c09
Rohitkumar-118 <rohitkumar2002811@gmail.com>
2026-06-04 22:56:29 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const connectDB = require('./config/db');
 connectDB();
 
 const app = express();
-app.use(cors({ origin: ['http://localhost:3000', 'http://127.0.0.1:3000',' https://sports-3duu.onrender.com'], credentials: true }));
+app.use(cors({ origin: ['http://localhost:3000', 'http://127.0.0.1:3000',' https://sports-sjhz.vercel.app'], credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
